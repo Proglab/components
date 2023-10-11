0fc0d5d1c5f36ada803fc45f9d6bb66f042b364c
Fabrice Gyre <info@proglab.com>
2023-10-11 17:09:40 +0200

    ✨Modal component

---
+++ src/Components/Modal/Modal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Proglab\Components\Components\Modal;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+
+#[AsTwigComponent('proglab_modal', template: '@ProglabComponents/components/modal/modal.html.twig')]
+class Modal
+{
+    public ?string $id = null;
+}
